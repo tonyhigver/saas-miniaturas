43f29883998cecd0c7cf9510f7aef6253a981126
tonyhigver <tonyhigver@gmail.com>
2025-09-15 19:53:29 +0200

    Integrada barra lateral en SaaSMain con enlaces a Miniaturas, CTR y Creador

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,31 +1,41 @@
+// components/Sidebar.js
 import { useRouter } from "next/router"
 
 export default function Sidebar() {
   const router = useRouter()
 
   return (
-    <div className="fixed top-0 left-0 h-full bg-gray-900 text-white w-16 hover:w-64 transition-all duration-300 shadow-lg flex flex-col">
+    <div className="fixed top-0 left-0 h-full bg-gray-900 text-white w-16 hover:w-64 transition-all duration-300 shadow-lg flex flex-col overflow-hidden group">
       <div className="flex flex-col mt-10">
         <button
           onClick={() => router.push("/create-miniature")}
           className="flex items-center gap-2 p-4 hover:bg-gray-700"
         >
           <span className="material-icons">image</span>
-          <span className="hidden md:inline">Miniaturas</span>
+          {/* Texto aparece al hacer hover en la barra */}
+          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+            Miniaturas
+          </span>
         </button>
+
         <button
           onClick={() => router.push("/ctr-prediction")}
           className="flex items-center gap-2 p-4 hover:bg-gray-700"
         >
           <span className="material-icons">show_chart</span>
-          <span className="hidden md:inline">CTR</span>
+          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+            CTR
+          </span>
         </button>
+
         <button
           onClick={() => router.push("/ab-test")}
           className="flex items-center gap-2 p-4 hover:bg-gray-700"
         >
           <span className="material-icons">compare_arrows</span>
-          <span className="hidden md:inline">A/B Test</span>
+          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 whitespace-nowrap">
+            A/B Test
+          </span>
         </button>
       </div>
     </div>
