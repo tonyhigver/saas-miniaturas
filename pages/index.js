5c1efe3a3fdff89019fda9df1bdae70a10b44ce3
tonyhigver <tonyhigver@gmail.com>
2025-09-15 18:27:32 +0200

    Primer commit limpio: Next.js + Tailwind

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold underline">
+        Hola SaaS 🚀
+      </h1>
+    </main>
+  )
+}
